96edd17baa52d51c24003461c8578fb9034262ac
Julian Nguyen <jndnguyen@ucdavis.edu>
2021-10-24 16:07:01 -0700

    fix indenting and refactor preprocessing
    uses tabs instead of spaces

--- processed_data/preprocessing.py
+++ processed_data/preprocessing.py
@@ -3,24 +3,8 @@ import numpy as np
 import csv
 from PIL import Image
 
-COVID_PATH = "../Images-processed/COVID/Useable"
-NONCOVID_PATH = "../Images-processed/CT_NonCOVID/Usable"
-
-# Preprocess all our images in specified folders
-# Return a dictionary mapping directory path to preprocessed image objects
-def preprocess_images(dirs: list) -> dict:
-	mean_width, mean_height = get_mean_dimensions(dirs)
-    
-    # Preprocess each image
-	preprocessed_images = {}
-	for dir in dirs:
-		image_names = os.listdir(dir)
-		preprocessed_images[dir] = []
-		for name in image_names:
-			processed_image = preprocess_image(f"{dir}/{name}", mean_width, mean_height)
-			preprocessed_images[dir].append(processed_image)
-	
-	return preprocessed_images
+COVID_PATH = "../raw_data/COVID/Useable"
+NONCOVID_PATH = "../raw_data/CT_NonCOVID/Usable"
 
 # Output the average width and height of all images in specified folders
 def get_mean_dimensions(dirs: list) -> list:
@@ -46,36 +30,42 @@ def preprocess_image(image_path: str, width: int, height: int) -> Image:
 	image = image.convert("L")
 	return image
 
-# Generate CSV file of preprocessed images (pixels + labels)
-# CSV file is our dataset to be used for training
-# positive_dirs: Images in these directories will be labelled as 1 in dataset
-def create_dataset(preprocessed_images: dict, positive_dirs: list):
-    images_pixel_format = []
-    
-    header = [f"pixel{i+1}" for i in range(width*height)] + ["class"]
-    csv_file.append(header)
-
-	# Create list of image pixels    
-    for dirname, images in preprocessed_images.items():
-    	for image in images:
-    		# Convert images to list of pixel values
-    		pixels = list(image.getdata())
-    		if positive_dirs.count(dirname) > 0:
-    			# These images are positive, so mark them
-    			class_value = 1
-    		else:
-    			class_value = 0
-    		pixels = pixels.append(class_value)
-    		
-    		# Add example to dataset
-    		images_pixel_format.append(pixels)
-
-	# Parse list of image pixels into csv file
-    with open("full_data.csv", "w+") as csvfile:
-        csvWriter = csv.writer(csvfile, delimiter=',')
-        csvWriter.writerows(images_pixel_format)
-
+# Preprocess images and generate dataset as CSV file
 if __name__ == '__main__':
-	preprocessed_images = preprocess_images([COVID_PATH, NONCOVID_PATH])
-	create_dataset(preprocessed_images, positive_dirs=[COVID_PATH])
+	csv_rows = []
 
+	image_dirs = [COVID_PATH, NONCOVID_PATH]
+	positive_dirs = [COVID_PATH]
+
+	mean_width, mean_height = get_mean_dimensions(image_dirs)
+
+	# Create CSV header
+	num_pixels = mean_width * mean_height
+	header = [f"pixel{i+1}" for i in range(num_pixels)]
+	header.append("class")
+	csv_rows.append(header)
+
+	for dir_name in image_dirs:
+		image_names = os.listdir(dir_name)
+	    # Preprocess each image and add as row to CSV
+		for image_name in image_names:
+			processed_image_obj = preprocess_image(f"{dir_name}/{image_name}", mean_width, mean_height)
+			
+			# Convert image object to list of pixel values
+			pixels = list(processed_image_obj.getdata())
+
+			# Include image class in each row
+			if positive_dirs.count(dir_name) > 0:
+				# This image is positive
+				class_value = 1
+			else:
+				class_value = 0
+			pixels.append(class_value)
+
+    		# Add example to dataset
+			csv_rows.append(pixels)
+
+	# Parse list of rows into csv file
+	with open("full_data.csv", "w+") as csvfile:
+		csv_writer = csv.writer(csvfile, delimiter=',')
+		csv_writer.writerows(csv_rows)
