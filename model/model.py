01257285083ad04b6046bed479b61f54cf5e29ae
Julian Nguyen <jndnguyen@ucdavis.edu>
2021-10-24 18:20:16 -0700

    finish model.py

--- model/model.py
+++ model/model.py
@@ -5,17 +5,17 @@ from tensorflow.keras.optimizers import SGD
 from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 
-
-def create_model(learning_rate, num_layers, num_nodes, input_dim):
+def create_model(learning_rate, num_layers, hidden_nodes, input_dim):
     # Set up layers for the model
     model = Sequential()
 
     # Input layer
-    model.add(Dense(16, input_dim=input_dim, activation='sigmoid'))
-    # Hidden layer
-    for _ in range(num_layers):
-        model.add(Dense(num_nodes, activation='relu'))
-        model.add(Dense(num_nodes, activation='relu'))
+    model.add(Dense(input_dim, activation='sigmoid'))
+    
+    # Hidden layers
+    for node_count in hidden_nodes:
+        model.add(Dense(node_count, activation='relu'))
+        
     # Output layer
     model.add(Dense(1, activation='sigmoid'))
 
@@ -26,7 +26,8 @@ def create_model(learning_rate, num_layers, num_nodes, input_dim):
 
 if __name__ == '__main__':
     # load data and separate them into class and features
-    df = pd.read_csv("../data/full_data.csv")
+    # TODO: create module of file paths
+    df = pd.read_csv("../processed_data/full_data.csv")
     feature_data = df.drop("class", axis=1)
     class_data = df.iloc[:, df.shape[1]-1]
 
@@ -34,13 +35,17 @@ if __name__ == '__main__':
     new_model = KerasClassifier(build_fn=create_model, verbose=0)
 
     # define the grid search parameters
-    num_layers = [3, 6, 12]
-    num_nodes = [3, 6, 12]
+    # num_layers = [3, 6, 12]
+    # num_nodes = [3, 6, 12]
+    hidden_nodes = [(1000, 50, 5), (1500, 100, 10), (2000, 200, 20, 5)]
     learning_rate = [0.1, 0.3, 0.5]
-    epochs = [1, 5, 10]
+    epochs = [10, 100, 500]
     input_dim = [feature_data.shape[1]]
-    param_grid = dict(input_dim=input_dim, num_layers=num_layers, num_nodes=num_nodes, learning_rate=learning_rate, epochs=epochs)
+    
+    param_grid = dict(input_dim=input_dim, hidden_nodes=hidden_nodes, learning_rate=learning_rate, epochs=epochs)
     grid = GridSearchCV(estimator=new_model, param_grid=param_grid, n_jobs=-1, cv=3)
+    
+    # Train model
     grid_result = grid.fit(feature_data, class_data)
 
     # summarize results
