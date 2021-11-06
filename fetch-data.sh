595080e2567bd67b43a59cd0869f8d6136f3206e
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-06 12:19:41 -0700

    sort training, testing, validation data into classes

--- fetch-data.sh
+++ fetch-data.sh
@@ -17,25 +17,6 @@ check_and_remove_dir(){
 	fi
 }
 
-# number_imgs(){
-# 	cd $1
-# 	i=1
-# 	for file in *; do
-# 		test -e "$i.png"
-# 		if [[ $? -eq 0 ]]; then
-# 			# Indexed image already exists
-# 			# Change file name to avoid deleting it
-# 			echo $1
-# 			echo $file
-# 			mv "$i.png" "$i-1.png"
-# 		fi
-
-# 		mv "$file" "$i.png"
-# 		i=$((i+1))
-# 	done
-# 	cd ..
-# }
-
 # Assume zip files have these names
 POSITIVE_ZIP='CT_COVID.zip'
 NEGATIVE_ZIP='CT_NonCOVID.zip'
@@ -61,43 +42,6 @@ download_dataset(){
 	cd ..
 }
 
-split_data(){
-	cd $1
-
-	echo -n "Splitting $1 data..."
-
-	# Get number of images
-	num_imgs=$(ls -l | wc -l)
-	num_imgs=$((num_imgs - 1))
-
-	files=(*)
-
-	# Training data
-	mkdir training
-	end_idx=$((num_imgs / 2))
-	for ((i = 0 ; i < end_idx ; i++)); do
-		mv "${files[$i]}" "training"
-	done
-
-	# Testing data
-	mkdir testing
-	start_idx=$((num_imgs / 2 + 1))
-	end_idx=$((num_imgs * 3 / 4))
-	for ((i = start_idx ; i < end_idx ; i++)); do
-		mv "${files[$i]}" "testing"
-	done
-
-	# Validation data
-	# Redirect errors in case files don't exist
-	mkdir validation
-	mv *.png validation
-	mv *.jpg validation
-
-	echo "done."
-
-	cd ..
-}
-
 clean_dataset(){
 	cd $1
 
@@ -113,19 +57,79 @@ clean_dataset(){
 		mv Usable CT_NonCOVID
 	fi
 
-	# Number all the images
-	# number_imgs 'CT_COVID'
-	# number_imgs 'CT_NonCOVID'
-
-	# Split into training, testing, and validation data
-	split_data 'CT_COVID'
-	split_data 'CT_NonCOVID'
-
 	# Remove useless metadata
 	check_and_remove_dir __MACOSX
 
 	# Don't need zip files anymore
 	rm $POSITIVE_ZIP $NEGATIVE_ZIP
+
+	cd ..
+}
+
+create_dataset_dir(){
+	mkdir $1
+	cd $1
+
+	mkdir CT_COVID
+	mkdir CT_NonCOVID
+
+	cd ..
+}
+
+sort_class_imgs(){
+	cd $1
+
+	files=(*)
+	num_imgs=${#files[@]}
+
+	# Move training data to the right folder
+	end_idx=$((num_imgs / 2))
+	for ((i = 0 ; i < end_idx ; i++)); do
+		mv "${files[$i]}" "../training/$1"
+	done
+
+	# Testing data
+	start_idx=$((num_imgs / 2 + 1))
+	end_idx=$((num_imgs * 3 / 4))
+	for ((i = start_idx ; i < end_idx ; i++)); do
+		mv "${files[$i]}" "../testing/$1"
+	done
+
+	# Validation data
+	mv *.{png,jpg} "../validation/$1"
+
+	cd ..
+}
+
+# data directory should look like this:
+# data
+# - training
+# -- CT_COVID
+# -- CT_NonCOVID
+# - testing
+# -- CT_COVID
+# -- CT_NonCOVID
+# - validation
+# -- CT_COVID
+# -- CT_NonCOVID
+# Each set has the class so image generators can distinguish them
+split_data(){
+	cd $1
+
+	echo -n "Splitting $1 data..."
+
+	create_dataset_dir training
+	create_dataset_dir testing
+	create_dataset_dir validation
+
+	sort_class_imgs CT_COVID
+	sort_class_imgs CT_NonCOVID
+
+	# No longer using image directories
+	rmdir CT_COVID
+	rmdir CT_NonCOVID
+
+	echo "done."
 
 	cd ..
 }
@@ -140,9 +144,11 @@ MODIFIED_LINK='https://github.com/ECS171-Team-15/Preprocessed-Dataset/raw/master
 ORIGINAL_LINK='https://github.com/UCSD-AI4H/COVID-CT/raw/master/Images-processed'
 
 download_dataset $MODIFIED_LINK modified
-download_dataset $ORIGINAL_LINK original
-
 clean_dataset modified
+split_data modified
+
+download_dataset $ORIGINAL_LINK original
 clean_dataset original
+split_data original
 
 echo "Script finished."
