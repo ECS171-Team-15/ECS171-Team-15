2f7b5d572bd341d726999d9e9bd8d743ee3f230b
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-06 10:58:37 -0700

    script splits images into validation, testing, and training

--- fetch-data.sh
+++ fetch-data.sh
@@ -17,20 +17,30 @@ check_and_remove_dir(){
 	fi
 }
 
-echo -n "Removing unclean data..."
-RAW_DATA_PATH='./raw_data'
-check_and_remove_dir $RAW_DATA_PATH
+# number_imgs(){
+# 	cd $1
+# 	i=1
+# 	for file in *; do
+# 		test -e "$i.png"
+# 		if [[ $? -eq 0 ]]; then
+# 			# Indexed image already exists
+# 			# Change file name to avoid deleting it
+# 			echo $1
+# 			echo $file
+# 			mv "$i.png" "$i-1.png"
+# 		fi
 
-echo "done."
-
-mkdir $RAW_DATA_PATH
-cd $RAW_DATA_PATH
+# 		mv "$file" "$i.png"
+# 		i=$((i+1))
+# 	done
+# 	cd ..
+# }
 
 # Assume zip files have these names
 POSITIVE_ZIP='CT_COVID.zip'
 NEGATIVE_ZIP='CT_NonCOVID.zip'
 
-retrieve_dataset(){
+download_dataset(){
 	echo -n "Downloading $2 data..."
 
 	# Make directory for this dataset
@@ -44,15 +54,55 @@ retrieve_dataset(){
 	echo "done."
 
 	echo -n "Extracting $2 data..."
-
 	unzip $POSITIVE_ZIP &> /dev/null
 	unzip $NEGATIVE_ZIP &> /dev/null
+	echo "done."
+
+	cd ..
+}
+
+split_data(){
+	cd $1
+
+	echo -n "Splitting $1 data..."
+
+	# Get number of images
+	num_imgs=$(ls -l | wc -l)
+	num_imgs=$((num_imgs - 1))
+
+	files=(*)
+
+	# Training data
+	mkdir training
+	end_idx=$((num_imgs / 2))
+	for ((i = 0 ; i < end_idx ; i++)); do
+		mv "${files[$i]}" "training"
+	done
+
+	# Testing data
+	mkdir testing
+	start_idx=$((num_imgs / 2 + 1))
+	end_idx=$((num_imgs * 3 / 4))
+	for ((i = start_idx ; i < end_idx ; i++)); do
+		mv "${files[$i]}" "testing"
+	done
+
+	# Validation data
+	# Redirect errors in case files don't exist
+	mkdir validation
+	mv *.png validation
+	mv *.jpg validation
 
 	echo "done."
 
-	# Only save useable folders in modified dataset
-	if [[ $2 == 'modified' ]]; then
-		# Make sure they are formatted properly
+	cd ..
+}
+
+clean_dataset(){
+	cd $1
+
+	# Only use useable images in modified dataset
+	if [[ $1 == 'modified' ]]; then
 		# Positive data
 		mv CT_COVID/Useable .
 		rm CT_COVID -rf
@@ -63,19 +113,36 @@ retrieve_dataset(){
 		mv Usable CT_NonCOVID
 	fi
 
-	# Don't need zip files anymore
-	rm $POSITIVE_ZIP $NEGATIVE_ZIP
+	# Number all the images
+	# number_imgs 'CT_COVID'
+	# number_imgs 'CT_NonCOVID'
+
+	# Split into training, testing, and validation data
+	split_data 'CT_COVID'
+	split_data 'CT_NonCOVID'
 
 	# Remove useless metadata
 	check_and_remove_dir __MACOSX
 
+	# Don't need zip files anymore
+	rm $POSITIVE_ZIP $NEGATIVE_ZIP
+
 	cd ..
 }
+
+# Remove data folder
+DATA_PATH='./data'
+check_and_remove_dir $DATA_PATH
+mkdir $DATA_PATH
+cd $DATA_PATH
 
 MODIFIED_LINK='https://github.com/ECS171-Team-15/Preprocessed-Dataset/raw/master'
 ORIGINAL_LINK='https://github.com/UCSD-AI4H/COVID-CT/raw/master/Images-processed'
 
-retrieve_dataset $MODIFIED_LINK modified
-retrieve_dataset $ORIGINAL_LINK original
+download_dataset $MODIFIED_LINK modified
+download_dataset $ORIGINAL_LINK original
+
+clean_dataset modified
+clean_dataset original
 
 echo "Script finished."
