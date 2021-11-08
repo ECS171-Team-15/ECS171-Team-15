ca02431f6f6b827145f2e01adf4472ac435064f8
Julian Nguyen <jndnguyen@ucdavis.edu>
2021-11-08 09:28:08 -0800

    add dropout

--- model/con_model.py
+++ model/con_model.py
@@ -12,6 +12,7 @@ def build_conv_model(train_x, train_y, test_x, test_y, input_dim):
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
