c58a877b4ba0f52b2eb869192bc2f1c76eebeb35
beam-ntth <beam4318.t@gmail.com>
2021-11-08 12:54:11 -0800

    added L1, L2, MinMaxScaler

--- model/con_model.py
+++ model/con_model.py
@@ -1,22 +1,24 @@
 import tensorflow
 import pandas as pd
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 def build_conv_model(train_x, train_y, test_x, test_y, input_dim):
     print("Building model...", end=" ")
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_dim))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
     model.compile(optimizer='adam',
-              loss=tensorflow.keras.losses.BinaryCrossentropy(from_logits=True),
+              loss=tensorflow.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
     model.fit(train_x, train_y, epochs=10, validation_data=(test_x, test_y), verbose=1)
     print("Done.")
@@ -39,13 +41,12 @@ def parse_test_train():
     return (train_x, train_y, test_x, test_y)
 
 if __name__ == "__main__":
-    #gpus = tensorflow.config.list_physical_devices('GPU')
-    #if gpus:
-    #    try:
-    #        tensorflow.config.experimental.set_memory_growth(gpus[0], True)
-    #    except RuntimeError as e:
-    #        print(e)
     train_x, train_y, test_x, test_y = parse_test_train()
+    scaler = MinMaxScaler()
+    print("Normalizing data...", end=" ")
+    train_x = scaler.fit_transform(train_x)
+    test_x = scaler.fit_transform(test_x)
+    print("Done.")
     train_x = tensorflow.reshape(train_x, (-1, 302, 425, 1))
     test_x = tensorflow.reshape(test_x, (-1, 302, 425, 1))
     model = build_conv_model(train_x, train_y, test_x, test_y, (302, 425, 1))
