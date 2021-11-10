f739b6d96d3cba1418ce2c7031d260a3e62bf087
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-09 18:19:36 -0800

    trying to add augmentation

--- model/con_model.py
+++ model/con_model.py
@@ -5,23 +5,42 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 def build_conv_model(train_x, train_y, test_x, test_y, input_dim):
-    print("Building model...", end=" ")
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_dim))
+    # Image augmentation
+    # Only applied to our training data
+    model = models.Sequential([
+        layers.InputLayer(input_shape=input_dim),
+        layers.experimental.preprocessing.RandomFlip(),
+        # 36 degree rotation range in both directions
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        # Zoom range for both width & height: -10% to 10%
+        # We want to fill the edges with black if we zoom out
+        layers.experimental.preprocessing.RandomZoom(0.1, fill_mode="constant"),
+        # Stretch image vertically by [-10%, 10%]
+        layers.experimental.preprocessing.RandomHeight(0.1),
+        # Do the same horizontally
+        layers.experimental.preprocessing.RandomWidth(0.1)
+    ])
+
+    # Convolution layers
+    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
     model.add(layers.Flatten())
+    
+    # Scramble some of the results to avoid overfitting
     model.add(layers.Dropout(0.5))
+    
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
     model.compile(optimizer='adam',
               loss=tensorflow.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
+    model.summary()
+
     model.fit(train_x, train_y, epochs=50, validation_data=(test_x, test_y), verbose=1)
-    print("Done.")
     return model
 
 def eval_model(test_x, test_y, model):
@@ -30,27 +49,28 @@ def eval_model(test_x, test_y, model):
     return
 
 def parse_test_train():
-    print("Loading data...", end=" ")
+    # Load data
     df = pd.read_csv('../processed_data/original.csv')
-    print("Done.")
     feature_data = df.drop(columns='class')
     label_data = df.iloc[:, df.shape[1]-1]
-    print("Splitting data...", end=" ")
+
+    # Split data
     train_x, test_x, train_y, test_y = train_test_split(feature_data, label_data, test_size=0.2, random_state=6)
-    print("Done.")
     return (train_x, train_y, test_x, test_y)
 
 if __name__ == "__main__":
     train_x, train_y, test_x, test_y = parse_test_train()
+    
+    # Scale data
     scaler = MinMaxScaler()
-    print("Normalizing data...", end=" ")
     train_x = scaler.fit_transform(train_x)
     test_x = scaler.fit_transform(test_x)
-    print("Done.")
+
+    # Train model
     train_x = tensorflow.reshape(train_x, (-1, 302, 425, 1))
     test_x = tensorflow.reshape(test_x, (-1, 302, 425, 1))
     model = build_conv_model(train_x, train_y, test_x, test_y, (302, 425, 1))
-    print("Save model...", end=" ")
     model.save("./model.h5")
-    print("Done.")
+
+    # Report results
     eval_model(test_x, test_y, model)
