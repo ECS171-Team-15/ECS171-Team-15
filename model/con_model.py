5db9c52ee80a824fa270c72d0de586e1bcfbdffc
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-11 18:37:38 -0800

    add gridsearch for cnn

--- model/con_model.py
+++ model/con_model.py
@@ -3,7 +3,7 @@ from tensorflow.keras import layers, models
 from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 
-from . import common
+import common
 
 def build_conv_model(input_dim, kernel_regularizer, dropout):
     model = models.Sequential()
@@ -43,21 +43,24 @@ if __name__ == "__main__":
 
     # Model hyperparameters
     param_grid = {
-        # Manually set the mean dimensions of the original dataset
-        # 3rd item in tuple is the number of channels
-        # Only 1 channel for grayscaleKerasClassifier
-        'input_dim': [(302, 425, 1)],
-        'dropout': [False, True],
-        'kernel_regularizer': [None, 'l1_l2'],
-    },
-
-    fit_params = {
-        'epochs': [100]
+        'dropout': [
+                    False,
+                    True
+                   ],
+        'kernel_regularizer': [
+                               None,
+                               'l1_l2'
+                              ],
     }
 
-    # Train model
-    model = KerasClassifier(build_fn=build_conv_model)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
-    grid_results = grid.fit(train_x, train_y, fit_params)
+    for dropout in param_grid['dropout']:
+        for kernel_regularizer in param_grid['kernel_regularizer']:
+            print("Running model with dropout:", dropout, "and kernel_regularizer:", kernel_regularizer)
+            # Manually set the mean dimensions of the original dataset
+            # 3rd item in tuple is the number of channels
+            # Only 1 channel for grayscaleKerasClassifier
+            model = build_conv_model((302, 425, 1), kernel_regularizer, dropout)
+            model.fit(train_x, train_y, epochs=100)
+            results = model.evaluate(test_x, test_y)
+            print("Test loss and accuracy", results)
 
-    common.print_grid_results(grid_results)
