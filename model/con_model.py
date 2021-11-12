df697367678fb944d54895678fd13619605de2d4
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-11 17:40:21 -0800

    gridsearch both CNN and DNN
    DNN code is broken

--- model/con_model.py
+++ model/con_model.py
@@ -1,56 +1,63 @@
 import tensorflow
-import pandas as pd
 from tensorflow.keras import layers, models
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
 
-def build_conv_model(train_x, train_y, test_x, test_y, input_dim):
-    print("Building model...", end=" ")
+from . import common
+
+def build_conv_model(input_dim, kernel_regularizer, dropout):
     model = models.Sequential()
+
+    # Convolutional layers
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_dim))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=kernel_regularizer))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l1_l2'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=kernel_regularizer))
     model.add(layers.Flatten())
-    model.add(layers.Dropout(0.5))
+
+    if dropout:
+        model.add(layers.Dropout(0.5))
+
+    # Classifier layers
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
+    # Default learning rate=0.001
+    # Source: https://keras.io/api/optimizers/adam/
     model.compile(optimizer='adam',
               loss=tensorflow.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
-    model.fit(train_x, train_y, epochs=50, validation_data=(test_x, test_y), verbose=1)
-    print("Done.")
     return model
 
-def eval_model(test_x, test_y, model):
-    test_loss, test_acc = model.evaluate(test_x,  test_y, verbose=2)
-    print(f"Loss is: {test_loss}\tAccuracy is: {test_acc}")
-    return
-
-def parse_test_train():
-    print("Loading data...", end=" ")
-    df = pd.read_csv('../processed_data/original.csv')
-    print("Done.")
-    feature_data = df.drop(columns='class')
-    label_data = df.iloc[:, df.shape[1]-1]
-    print("Splitting data...", end=" ")
-    train_x, test_x, train_y, test_y = train_test_split(feature_data, label_data, test_size=0.2, random_state=6)
-    print("Done.")
-    return (train_x, train_y, test_x, test_y)
-
 if __name__ == "__main__":
-    train_x, train_y, test_x, test_y = parse_test_train()
-    scaler = MinMaxScaler()
-    print("Normalizing data...", end=" ")
-    train_x = scaler.fit_transform(train_x)
-    test_x = scaler.fit_transform(test_x)
-    print("Done.")
+    # Load data and separate class and features
+    train_x, test_x, train_y, test_y = common.load_and_split_data()
+
+    # Scale data to [0, 1] range
+    train_x, test_x = common.rescale_data(train_x, test_x)
+
+    # Reshape data to 2D
     train_x = tensorflow.reshape(train_x, (-1, 302, 425, 1))
     test_x = tensorflow.reshape(test_x, (-1, 302, 425, 1))
-    model = build_conv_model(train_x, train_y, test_x, test_y, (302, 425, 1))
-    print("Save model...", end=" ")
-    model.save("./model.h5")
-    print("Done.")
-    eval_model(test_x, test_y, model)
+
+    # Model hyperparameters
+    param_grid = {
+        # Manually set the mean dimensions of the original dataset
+        # 3rd item in tuple is the number of channels
+        # Only 1 channel for grayscaleKerasClassifier
+        'input_dim': [(302, 425, 1)],
+        'dropout': [False, True],
+        'kernel_regularizer': [None, 'l1_l2'],
+    },
+
+    fit_params = {
+        'epochs': [100]
+    }
+
+    # Train model
+    model = KerasClassifier(build_fn=build_conv_model)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=3)
+    grid_results = grid.fit(train_x, train_y, fit_params)
+
+    common.print_grid_results(grid_results)
