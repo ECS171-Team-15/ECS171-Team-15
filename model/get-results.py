f860949086184316d493eb8641591e6276ddf4eb
Natheethorn Teacharuangchit <beam4318.t@gmail.com>
2021-11-15 19:14:07 -0800

    Fixed rounding problem + Added confusion matrix

--- model/get-results.py
+++ model/get-results.py
@@ -1,24 +1,36 @@
+import os
+import tensorflow
+import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import load_model
-import os
 from sklearn.preprocessing import MinMaxScaler
-import tensorflow
-from sklearn.metrics import classification_report
-import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
 
 import common
 
 def eval_model(model_filename, test_x, test_y):
 	model = load_model(model_filename)
-	predicted_y = model.predict(test_x)
-	print(predicted_y)
-	print(test_y)
-	# predicted_y = np.asarray(predicted_y)
-	# print(type(predicted_y), type(test_y))
+        predicted_y = model.predict(test_x)
+        predicted_y = [round(y[0], 0) for y in predicted_y]
+        # print(predicted_y)
+        # print(test_y)
 	report = classification_report(test_y, predicted_y)
 
-	print(f"{model_filename} results:")
-	print(report)
+        print(f"CLASSIFICATION REPORT: {model_filename} results:\n")
+        print(report)
+        cm = confusion_matrix(test_y, predicted_y)
+        print(f"\nCONFUSION MATRIX: {model_filename} results:\n")
+	
+	# Display confusion matrix
+        print("\t\tNON-COVID\tCOVID")
+        for row in range(len(cm)):
+                if row == 0:
+                        print("NON-COVID", end="\t")
+                else:
+                        print("COVID", end="\t\t")
+                for col in cm[row]:
+                        print(col, end="\t\t")
+                print("\n")
 
 if __name__ == '__main__':
 	_, test_x, _, test_y = common.load_and_split_data()
