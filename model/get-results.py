0ae7c95c4e21d3ca12eab3e6ca7058a01e9c44de
ProphecyRox <72328335+ProphecyRox@users.noreply.github.com>
2021-11-15 17:52:51 -0800

    wip classification report on model

--- model/get-results.py
+++ model/get-results.py
@@ -3,14 +3,22 @@ from keras.models import load_model
 import os
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow
+from sklearn.metrics import classification_report
+import numpy as np
 
 import common
 
 def eval_model(model_filename, test_x, test_y):
 	model = load_model(model_filename)
-	results = model.evaluate(test_x, test_y)
-	print(f"{model_filename} results")
-	print(results)
+	predicted_y = model.predict(test_x)
+	print(predicted_y)
+	print(test_y)
+	# predicted_y = np.asarray(predicted_y)
+	# print(type(predicted_y), type(test_y))
+	report = classification_report(test_y, predicted_y)
+
+	print(f"{model_filename} results:")
+	print(report)
 
 if __name__ == '__main__':
 	_, test_x, _, test_y = common.load_and_split_data()
