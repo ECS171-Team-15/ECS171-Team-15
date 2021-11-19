c17dbe0048a18755d3467e1de5688b268c2dc65d
bchou9 <bchou9@users.noreply.github.com>
2021-11-19 13:23:00 -0800

    add working dnn model

--- model/model.py
+++ model/model.py
@@ -1,81 +1,87 @@
 import time
 import numpy as np
 import sys
-import common
 
+import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, InputLayer
 from tensorflow.keras.optimizers import SGD
+from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.preprocessing import MinMaxScaler
 
-# import tensorflow.config
+DATASET_PATH = '../processed_data/full_data.csv'
 
-def build_dnn_model(input_dim, learning_rate, hidden_nodes):
+def create_model(learning_rate, hidden_nodes, input_dim):
+    # Set up layers for the model
     model = Sequential()
+
+    # Input layer
+    model.add(InputLayer(input_dim))
 
     # Hidden layers
     for i in range(0, len(hidden_nodes)):
-        if i == 0:
-            model.add(Dense(hidden_nodes[i], activation='relu', input_dim=input_dim))
-            continue
         model.add(Dense(hidden_nodes[i], activation='relu'))
+
     # Output layer
     model.add(Dense(1, activation='sigmoid'))
 
     opt = SGD(learning_rate=learning_rate)
-    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=opt , metrics=['accuracy'])
+
     return model
 
+# Pass in # of hidden layer nodes as arguments
+# Usage example: python3 model.py 1000 100 20 5
 if __name__ == '__main__':
+    print("GPU Test:" + tensorflow.version.VERSION)
+    # Save the start time to calculate the total running time
+    start_time = time.time()
+
     # Limit memory usage of GPU
-    '''
-    gpus = tensorflow.config.list_physical_devices('GPU')
-    if gpus:
-        try:
-            tensorflow.config.experimental.set_memory_growth(gpus[0], True)
-        except RuntimeError as e:
-            print(e)
-    '''
+    tensorflow.config.list_physical_devices()
+    #exit(1)
+
+    # Input validation
+    if len(sys.argv) < 2:
+        print("Usage: python3 model.py count1 count2 ...")
+        exit()
+
+    print('Opening dataset...', end='')
 
     # Load data and separate class and features
-    train_x, test_x, train_y, test_y = common.load_and_split_data()
+    df = pd.read_csv(DATASET_PATH)
+    feature_data = df.drop("class", axis=1)
+    class_data = df.iloc[:, df.shape[1]-1]
 
-    # Rescale data
-    train_x, test_x = common.rescale_data(train_x, test_x)
+    new_model = KerasClassifier(build_fn=create_model, verbose=0)
 
-    num_pixels_img = train_x.shape[1]
+    # Read hidden nodes from program arguments
+    hidden_nodes = []
+    for i in range(1, len(sys.argv)):
+        hidden_nodes.append(sys.argv[i])
 
-    # Models to train and their parameter grids
-    param_grid = {
-        'model': KerasClassifier(build_dnn_model),
-        'hp': {
-            # Tuple with one dimension size
-            'input_dim': (num_pixels_img,),
-            'learning_rate': [0.1, 0.3, 0.5],
-            # Ordered hidden node combinations
-            'hidden_nodes': [
-                [2000, 300, 40, 10],
-                [2500, 400, 50, 13]
-            ],
-        },
-        'fit_params': {
-            'epochs': 500
-        }
-    }
+	# Grid search parameters
+    learning_rate = [0.1, 0.3, 0.5]
+    epochs = [10, 100, 500]
+    input_dim = [feature_data.shape[1]]
+    param_grid = dict(input_dim=input_dim, hidden_nodes=hidden_nodes, learning_rate=learning_rate, epochs=epochs)
 
-    # Train models
-    for model_name, model_properties in models.items():
-        grid = GridSearchCV(estimator=model_properties['model'], param_grid=model_properties['hp'], n_jobs=1, cv=3)
+    grid = GridSearchCV(estimator=new_model, param_grid=param_grid, n_jobs=-1, cv=3)
 
-        # feature_data = np.asarray(feature_data).astype('float64')
-        # class_data = np.asarray(class_data).astype('float64')
+    # Train model
+    feature_data = np.asarray(feature_data).astype('float64')
+    class_data = np.asarray(class_data).astype('float64')
+    grid_result = grid.fit(feature_data, class_data)
 
-        if model_name == 'cnn':
-            print("Resize training image data into original 2D shape")
-        else:
-            # Data can be 1D for DNNs
-            train_x = tensorflow.reshape(train_x, (-1,))
-            test_x = tensorflow.reshape(test_x, (-1,))
+    # Results
+    print("MAX ACCURACY: %f using %s\n" % (grid_result.best_score_, grid_result.best_params_))
+    print("TRACEBACK:\n")
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("Accuracy: %f (STD: %f) with: %r" % (round(mean, 2), round(stdev, 4), param))
 
-        grid_result = grid.fit(training_x, training_y, fit_params=model_properties['fit_params'])
+    # Time elapsed
+    print()
+    print(f"Program time: {time.time() - start_time} seconds")
