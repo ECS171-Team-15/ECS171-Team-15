c565a49219f84c846d86392de18789f234dc0b92
HC <51252433+bchou9@users.noreply.github.com>
2021-11-20 23:21:13 -0800

    remove hard coded values, add function for graph

--- model/con_model.py
+++ model/con_model.py
@@ -10,6 +10,9 @@ CSV_PATH = "../processed_data/original.csv"
 TEST_DATA_SIZE = 0.2
 RANDOM_STATE = 77
 OUTPUT_DIM = 1
+MEAN_WIDTH = 302
+MEAN_HEIGHT = 425
+EPOCHS = 100
 
 def build_conv_model(input_dim, kernel_regularizer, dropout, dropout_rate=0.5):
     model = models.Sequential()
@@ -36,6 +39,47 @@ def build_conv_model(input_dim, kernel_regularizer, dropout, dropout_rate=0.5):
               metrics=['accuracy'])
     return model
 
+def plot_accuracy_and_loss(dropout, kernel_regularizer):
+    epochs = range(1, EPOCHS + 1)
+    title_kr_value = kernel_regularizer
+    if (kernel_regularizer == None):
+        title_kr_value = 'no'
+    if (dropout):
+        title_acc = f'Model performance (accuracy) with dropout and {title_kr_value} regularization'
+        title_loss = f'Model performance (loss) with dropout and {title_kr_value} regularization'
+
+    else:
+        title_acc = f'Model performance (accuracy) with no dropout and {title_kr_value} regularization'
+        title_loss = f'Model performance (loss) with no dropout and {title_kr_value} regularization'
+    img_name_loss = f'{dropout}{kernel_regularizer}_loss.png'
+    img_name_acc = f'{dropout}{kernel_regularizer}_acc.png'
+
+    # get the callbacks of the loss and accuracy metrics for each of the data set
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    train_acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    # 1) plot the loss for the given hp -> 'green' represents training and 'blue' represents validation
+    plt.plot(epochs, train_loss, 'g', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title(title_loss)
+    plt.legend()
+    plt.savefig(img_name_loss)
+    plt.show(block=False)
+
+    # 2) Plot the accuracy for the given HP -> 'green' represents training and 'blue' represents validation
+    plt.plot(train_acc, 'g', label='Training Accuracy')
+    plt.plot(val_acc, 'b', label='Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title(title_acc)
+    plt.legend()
+    plt.savefig(img_name_acc)
+    plt.show(block=False)
+
 if __name__ == "__main__":
     # Load data and separate class and features
     train_x, test_x, train_y, test_y = common.load_and_split_data(TEST_DATA_SIZE, RANDOM_STATE, CSV_PATH)
@@ -44,8 +88,8 @@ if __name__ == "__main__":
     train_x, test_x = common.rescale_data(train_x, test_x)
 
     # Reshape data to 2D
-    train_x = tensorflow.reshape(train_x, (-1, 302, 425, OUTPUT_DIM))
-    test_x = tensorflow.reshape(test_x, (-1, 302, 425, OUTPUT_DIM))
+    train_x = tensorflow.reshape(train_x, (-1, MEAN_WIDTH, MEAN_HEIGHT, OUTPUT_DIM))
+    test_x = tensorflow.reshape(test_x, (-1, MEAN_WIDTH, MEAN_HEIGHT, OUTPUT_DIM))
 
     # Model hyperparameters
     param_grid = {
@@ -63,49 +107,11 @@ if __name__ == "__main__":
             # Manually set the mean dimensions of the original dataset
             # 3rd item in tuple is the number of channels
             # Only 1 channel for grayscaleKerasClassifier
-            model = build_conv_model((302, 425, OUTPUT_DIM), kernel_regularizer, dropout)
-            history = model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=100)
+            model = build_conv_model((MEAN_WIDTH, MEAN_HEIGHT, OUTPUT_DIM), kernel_regularizer, dropout)
+            history = model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=EPOCHS)
             model.save(f'{dropout}{kernel_regularizer}.h5')
             results = model.evaluate(test_x, test_y)
             print("Test loss and accuracy", results)
 
-            #-------PLOT THE ACCURACY AND LOSS FOR EACH OF THE VAL AND TRAINING DATA----------#
-            epochs = range(1, 101)
-            title_kr_value = kernel_regularizer
-            if(kernel_regularizer == None):
-                title_kr_value = 'no'
-            if(dropout):
-                title_acc = f'Model performance (accuracy) with dropout and {title_kr_value} regularization'
-                title_loss = f'Model performance (loss) with dropout and {title_kr_value} regularization'
-
-            else:
-                title_acc = f'Model performance (accuracy) with no dropout and {title_kr_value} regularization'
-                title_loss = f'Model performance (loss) with no dropout and {title_kr_value} regularization'
-            img_name_loss = f'{dropout}{kernel_regularizer}_loss.png'
-            img_name_acc = f'{dropout}{kernel_regularizer}_acc.png'
-
-            #get the callbacks of the loss and accuracy metrics for each of the data set
-            train_loss = history.history['loss']
-            val_loss = history.history['val_loss']
-            train_acc = history.history['accuracy']
-            val_acc = history.history['val_accuracy']
-
-            #1) plot the loss for the given hp -> 'green' represents training and 'blue' represents validation
-            plt.plot(epochs, train_loss, 'g', label = 'Training Loss')
-            plt.plot(epochs, val_loss, 'b', label = 'Validation Loss')
-            plt.xlabel('Epochs')
-            plt.ylabel('Loss')
-            plt.title(title_loss)
-            plt.legend()
-            plt.savefig(img_name_loss)
-            plt.show(block=False)
-
-            #2) Plot the accuracy for the given HP -> 'green' represents training and 'blue' represents validation
-            plt.plot(train_acc, 'g', label = 'Training Accuracy')
-            plt.plot(val_acc, 'b', label = 'Validation Accuracy')
-            plt.xlabel('Epochs')
-            plt.ylabel('Accuracy')
-            plt.title(title_acc)
-            plt.legend()
-            plt.savefig(img_name_acc)
-            plt.show(block=False)
+            # Plot accuracy and loss for each combination of hyperparameters
+            plot_accuracy_and_loss(dropout, kernel_regularizer)
